f7d910e143dd46a4e8f1e9592af5ac3fd0f935d4
Anderson Duran <andersonduran@gmail.com>
2021-04-30 17:39:06 -0300

    renamed

--- src/main/kotlin/com/duranzote/Application.kt
+++ src/main/kotlin/com/duranzote/Application.kt
@@ -18,7 +18,7 @@ import org.redisson.config.Config
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
+@Suppress("unused")
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
